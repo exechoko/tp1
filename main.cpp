68be24c70ef29740dd3478248e2f50df0a9f1fbe
Exequiel <49988126+exechoko@users.noreply.github.com>
2019-04-29 23:34:07 -0300

    6 y 7 terminado

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-//#include <stdio.h>      /* printf, NULL */
+#include <stdio.h>      /* printf, NULL */
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>
-
 using namespace std;
 
 void matriz(int n, int m);
@@ -10,12 +9,13 @@ void cargar_matriz(int **&pmatriz,int n,int m);
 int areascerradas(int **&pmatriz,int n, int m, int &i, int &j, int &valor);
 bool validar(int **&pmatriz, int n, int m, int &i, int &j, int x);
 void mostrar(int **pmatriz, int n, int m, int x);
-void liberar(int **pmatriz, int n);
-//int areacerradas(int **&pmatriz, int &i, int &j, int &a);
-//bool validar(int **&pmatriz,int n, int m);
+void mostrar2(int **pmatriz, int n, int m, int x);
+void liberar(int **&pmatriz, int n);
+
 
 int main()
 {
+    int **pmatriz = NULL;
     int filas, columnas;
 
     cout << "Ingrese la cantidad de filas: ";
@@ -25,11 +25,13 @@ int main()
 
     matriz(filas,columnas);
 
+    liberar(pmatriz, filas);
+
     return 0;
 }
 
 void matriz(int n, int m){
-    int i, j, y;
+
     int **pmatriz = new int *[n];
 
     for(int i=0;i<n;i++){
@@ -38,14 +40,10 @@ void matriz(int n, int m){
 
     cargar_matriz(pmatriz, n, m);
 
-
-    //mostrar(pmatriz, n, m); Esta función está ahora dentro de la función "cargar_matriz"
-
-    liberar(pmatriz, n);
 }
 
 void cargar_matriz(int **&pmatriz,int n,int m){
-    int NUM, x, y, i, j;
+    int NUM, x, y;
 
     srand(time(0));
 
@@ -72,14 +70,17 @@ void cargar_matriz(int **&pmatriz,int n,int m){
               }
         }
     }
-     mostrar(pmatriz, n, m, x); //agregué el valor de x para que la funcion "validar" sea mas precisa
-    }
+    mostrar(pmatriz, n, m, x); //agregué el valor de x para que la funcion "validar" sea mas precisa
+    cout << endl;
+    //mostrar2(pmatriz, n, m, x);
+
+}
 
  //   areascerradas(pmatriz,n,m,i,j,y); Ahora está dentro de la función "validar"
 
 
 void mostrar(int **pmatriz, int n, int m, int x){
-    int valor, i, j;
+    int i, j;
     for(i=0; i<n; i++){
         for(j=0; j<m; j++){
             cout << pmatriz[i][j] << " ";
@@ -89,10 +90,24 @@ void mostrar(int **pmatriz, int n, int m, int x){
 
     if (validar(pmatriz,n,m,i,j,x) == true)
         cout << "Matriz Valida" << endl;
-    else cout << "Matriz Invalida" << endl;
+    else {
+        cout << "Matriz Invalida" << endl;
+        mostrar2(pmatriz, n, m, x);
+    }
 }
 
-void liberar(int **pmatriz, int n){
+void mostrar2(int **pmatriz, int n, int m, int x){
+    int i, j;
+    cout << "MATRIZ VALIDADA" << endl;
+    for(i=0; i<n; i++){
+        for(j=0; j<m; j++){
+            cout << pmatriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void liberar(int **&pmatriz, int n){
     for(int i=0; i<n; i++){
         delete[] pmatriz[i];
     }
@@ -101,176 +116,112 @@ void liberar(int **pmatriz, int n){
 
 int areascerradas(int **&pmatriz,int n, int m, int &i, int &j, int &valor){
     int band = 1;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-           int band2=0;
+    int band3 = 1;
+    for( i=0; i<n; i++){
+       for( j=0; j<m; j++){
+
+           int band = 1;
+           int band2= 0;
             if(pmatriz[i][j]==valor){
                 if (j==0 && i==0){
                     band2=1;
-                    if((valor == pmatriz[i+1][j] )|| (valor == pmatriz[i][j+1] )){
-                        band = 1;
-                    }
-                    else {
+                    if((valor != pmatriz[i+1][j]) && (valor != pmatriz[i][j+1] )){
                        band = 0;
                     }
                 }
                 if (j==(m-1) && i==(n-1)){
                     band2=1;
-                    if((valor == pmatriz[i-1][j])|| (valor == pmatriz[i][j-1] )){
-                        band = 1;
-                    }
-                    else {
+                    if((valor != pmatriz[i-1][j]) && (valor != pmatriz[i][j-1] )){
                         band = 0;
                     }
                 }
                 if (j==(m-1) && i==0){
                     band2=1;
-                    if((valor == pmatriz[i+1][j] )|| (valor == pmatriz[i][j-1])){
-                        band = 1;
-                    }
-                    else {
+                    if((valor != pmatriz[i+1][j]) && (valor != pmatriz[i][j-1])){
                         band =0;
                     }
                 }
                 if (j==0 && i==(n-1)){
                     band2=1;
-                    if((valor == pmatriz[i-1][j] ) || (valor == pmatriz[i][j+1])){
-                        band = 1;
-                    }
-                    else {
+                    if((valor != pmatriz[i-1][j]) && (valor != pmatriz[i][j+1])){
                         band =0;
                     }
                 }
                if(band2==0){
                        if (j==0){
-                           if((valor == pmatriz[i+1][j] )|| (valor == pmatriz[i][j+1] ) || (valor == pmatriz[i-1][j] )){
-                               band = 1;
-                           }
-                           else {
+                           band2=1;
+                           if((valor != pmatriz[i+1][j]) && (valor != pmatriz[i][j+1] ) && (valor != pmatriz[i-1][j] )){
                                band =0;
                            }
                        }
                        if (i==0){
-                           if((valor == pmatriz[i+1][j] )|| (valor == pmatriz[i][j+1] ) || (valor == pmatriz[i][j-1])){
-                               band = 1;
-                           }
-                           else {
+                           band2=1;
+                           if((valor != pmatriz[i+1][j]) && (valor != pmatriz[i][j+1] ) && (valor != pmatriz[i][j-1])){
                                band =0;
                            }
                        }
                        if (j==(m-1)){
-                           if((valor == pmatriz[i+1][j] )|| (valor == pmatriz[i][j-1]) || (valor == pmatriz[i-1][j])){
-                               band = 1;
-                           }
-                           else {
+                           band2=1;
+                           if((valor != pmatriz[i+1][j]) && (valor != pmatriz[i][j-1]) && (valor != pmatriz[i-1][j])){
                                band =0;
                            }
                        }
                        if (i==(n-1)){
-                           if((valor == pmatriz[i-1][j] )|| (valor == pmatriz[i][j-1]) || (valor == pmatriz[i][j+1])){
-                               band = 1;
+                           band2=1;
+                           if((valor != pmatriz[i-1][j]) && (valor != pmatriz[i][j-1]) && (valor != pmatriz[i][j+1])){
+                               band =0;
                            }
-                           else {
+                       }
+                       if(band2==0){
+                           if((valor != pmatriz[i-1][j]) && (valor != pmatriz[i][j-1]) && (valor != pmatriz[i][j+1]) && (valor != pmatriz[i+1][j])){
                                band =0;
                            }
                        }
                    }
                }
 
-                /*if ((valor == (pmatriz[i][j+1]  || pmatriz[i+1][j]))){
-                    band = 1;
-                } else {
-                    if ((valor == (pmatriz[i][j+1] || pmatriz[i+1][j]))){
-                        band = 1;
-                }} else{
-                    if ((valor == (pmatriz[i][j+1] || pmatriz[i][j-1] || pmatriz[i+1][j] || pmatriz[i-1][j]))){
-                        band = 1;
-                    } else{
-                band = 0;
-            }}*/
+
             if(band == 0){
-                i=n;
-                j=m;
+                cout << "INVALIDA en ["<< i << "][" << j << "]" << endl;
+                if ( (i==0) && (j==0) ){
+                    pmatriz [i][j+1] = valor; // cambia el valor invalido
+                    band3=0;}
+                else if ( (i==(n-1)) && (j==(m-1)) ){
+                        pmatriz [i][j-1] = valor; // cambia el valor invalido
+                        band3=0;}
+                    else if ( (i==0) && (j!=0) ){
+                            pmatriz [i][j-1] = valor; // cambia el valor invalido
+                            band3=0;}
+                        else if ( (i!=0) && (j==0) ){
+                                pmatriz [i-1][j] = valor; // cambia el valor invalido
+                                band3=0;}
+                            else if ( (i!=0) && (j!=0) ){
+                                      pmatriz [i][j-1] = valor; // cambia el valor invalido
+                                      band3=0;}
+                                 else if ( (i==0) && (j==(m-1)) ){
+                                           pmatriz [i][j-1] = valor; // cambia el valor invalido
+                                           band3=0;}
+                                       else if ( (i==(n-1)) && (j==0) ){
+                                               pmatriz [i-1][j] = valor; // cambia el valor invalido
+                                                band3=0;}
+                                              else if ( (i!=0) && (j==(m-1)) ){
+                                                      pmatriz [i-1][j] = valor; // cambia el valor invalido
+                                                      band3=0;}
+                                                    else if ( (i==(n-1)) && (j!=0) ){
+                                                            pmatriz [i][j-1] = valor; // cambia el valor invalido(funciona)
+                                                            band3=0;}
             }
-          }
-         }
+       }
+    }
 
-
-
-    return (band);
+    return (band3);
 }
 
 
 bool validar(int **&pmatriz, int n, int m, int &i, int &j, int x){
-    int valor;
-    //int band = 0;
-    //pmatriz[i][j] = valor;
-
-    /*for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if ((valor == (pmatriz[i][j+1]  || pmatriz[i+1][j])) && (i == 0)){
-                band = 1;
-            } else
-                if ((valor == (pmatriz[i][j+1] || pmatriz[i+1][j])) && (j == 0)){
-                        band = 1;
-            } else
-                    if ((valor == (pmatriz[i][j+1] || pmatriz[i][j-1] || pmatriz[i+1][j] || pmatriz[i-1][j])) && (i != 0) && (j != 0)){
-                        band = 1;
-            }
-        }
-     }*/
-
-
 
     if (areascerradas(pmatriz,n,m,i,j,x) == 1)
         return (true);
     else return (false);
 
 }
-
-
-
-
-
-/*if ((pmatriz[i][j] == pmatriz[i][j+1]) || (pmatriz[i][j] == pmatriz[i][j-1]) || (pmatriz[i][j] == pmatriz[i+1][j]) || pmatriz[i][j] == pmatriz[i-1][j])
-{
-    band=1;
-    cout << band << " valido" <<endl;}
-else  {
-    band=0;
-    cout << band << "No valido " << pmatriz[i][j] <<endl;
-}
-}
-
-int areacerradas(int **&pmatriz, int &i, int &j, int &a){
-    int band = 0;
-
-    if (pmatriz[i][j+1] == a)//pmatriz[i][j+1])
-            band=1;
-    else if (pmatriz[i][j-1] == a)//pmatriz[i][j-1])
-                band=1;
-           else if (pmatriz[i+1][j] == a)//pmatriz[i+1][j])
-                     band=1;
-                else if (pmatriz[i-1][j] == a)// pmatriz[i-1][j])
-                            band=1;
-                     else band=0;
-
-    return (band);
-}
-
-bool validar(int **&pmatriz,int n, int m){
-    int i=0, j=0, a;
-
-            areacerradas(pmatriz,i,j,a);
-
-
-
-    if (areacerradas(pmatriz,i,j,a) == 1)
-        return (true);
-    else return (false);
-
-}*/
-
-
-/*Número aleatorios entre 250 y 420: 9y 12
-  num=250+rand()%(421-250);*/
